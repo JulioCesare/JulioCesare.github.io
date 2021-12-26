d2b745c5255777f07c42a148cc9f510f880176d0
JulioCesare <59570657+JulioCesare@users.noreply.github.com>
2021-12-26 02:35:59 +0100

    hehe

---
+++ game.js
@@ -0,0 +1,128 @@
+var start = 0
+var fase = 0
+var total_points = 1
+var hehgarancy = 0;
+//Per la fase 0
+let nameOriginal = ['EMAU', 'ASON', 'NENA', 'ALUE', 'NASU']
+let names = ['EMAU', 'ASON', 'NENA', 'ALUE', 'NASU']
+let namesCounter = ['EMAU', 'ASON', 'NENA', 'ALUE', 'NASU']
+var name = 0
+var nameC = 0
+var lastname = "";
+//Per la fase 1
+var catNum = 0;
+var catName = "";
+//Per la fase 2
+var cardPicker = 0;
+var lastcard = 0;
+
+card.addEventListener("click", checkContinue);
+
+function checkContinue(){
+	document.getElementById("turns").innerHTML = "T "+total_points;
+	if (names.length == 0)
+	{
+		names = Array.from(nameOriginal);
+	}
+	if (namesCounter.length == 0){
+		namesCounter = Array.from(nameOriginal);
+	}
+	if  (start == 0)
+	{
+		fase = 0
+		start = 1
+		document.getElementById("prompt").style.visibility = "visible";
+	}
+	switch (fase){
+		case 0:
+			name = Math.floor(Math.random() * (names.length));
+			document.getElementById("prompt").innerHTML = names[name] + " TIME";
+			document.getElementById("card").src = "cards/char/" + names[name] + "char.png";
+			console.log("Siamo alla fase 0")
+			lastname = names[name]
+			names.splice(name,1);
+			fase++;
+			break;
+		case 1:
+			catNum = Math.floor((Math.random() * 101));
+			console.log(catNum);
+			if (catNum >= 0 && catNum <= 5){
+				catNum = 2;
+				catName = "URCards";
+				cardPicker = Math.floor((Math.random() * 125) + 1);
+				while (cardPicker == lastcard){
+					cardPicker = Math.floor((Math.random() * 125) + 1);
+				}
+			}
+			else if (catNum > 5 && catNum <= 80){
+				catNum = 0;
+				catName = "SfideCards";
+				cardPicker = Math.floor((Math.random() * 25) + 1);
+				while (cardPicker == lastcard){
+					cardPicker = Math.floor((Math.random() * 25) + 1);
+				}
+			}
+			else if (catNum > 80 && catNum <= 100){
+				catNum = 1;
+				catName = "EventCards";
+				cardPicker = Math.floor((Math.random() * 5) + 1);
+				while (cardPicker == lastcard){
+					cardPicker = Math.floor((Math.random() * 5) + 1);
+				}
+			}
+			document.getElementById("card").src = "cards/categ/" + catNum + "event.png";
+			lastcard = cardPicker;
+			fase++;
+			break;
+		case 2:
+			console.log("Siamo alla fase 2");
+			document.getElementById("card").src = "cards/prompt/" + catName + "/" + cardPicker + ".png";
+			fase++;
+			break;
+		case 3:
+			document.getElementById("card").src = "cards/char/versus/picker.gif";
+			fase++;
+			break;
+		case 4:
+			console.log("Siamo alla fase 3")
+			nameC = Math.floor(Math.random() * (namesCounter.length));
+			while(namesCounter[nameC] == lastname){
+				nameC = Math.floor(Math.random() * (namesCounter.length));
+				if (namesCounter[nameC] == lastname && namesCounter.length == 1){
+					hehgarancy = 1;
+					break;
+				}
+			}
+			if (!hehgarancy){	
+				document.getElementById("prompt").innerHTML = "Verso/con " + namesCounter[nameC] + ".";
+				document.getElementById("card").src = "cards/char/versus/" + namesCounter[nameC] + "char.png";
+			}
+			else{
+				document.getElementById("prompt").innerHTML = "Gioca con sé stesso.";
+				document.getElementById("card").src = "cards/char/versus/" + namesCounter[nameC] + "char.png";
+				hehgarancy=0;
+			}
+			namesCounter.splice(nameC,1);
+			fase = 0;
+			total_points++;
+			lastname = ""
+			break;
+		}
+	/*if(fase == 0)
+	{
+		
+	}
+	else if (fase == 1)
+	{
+		
+	}
+	else if (fase == 2){
+		
+	}
+	else if (fase == 3){
+		
+	}
+	else if (fase == 4){
+		
+	}*/
+}
